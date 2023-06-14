d603e3908537b3a84e7ef5bd28f0e73e7e9ebfbd
sandraoliv <sandra.so219@gmail.com>
2023-06-14 14:36:18 -0300

    refactor

--- users/serializers.py
+++ users/serializers.py
@@ -7,11 +7,12 @@ class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     email = serializers.EmailField(max_length=127)
     username = serializers.CharField(max_length=50)
-    last_name = serializers.CharField(max_length=50)
-    birthdate = serializers.DateField(allow_null=True, default=None)
-    is_employee = serializers.BooleanField(default=False)
-    is_superuser = serializers.BooleanField(read_only=True)
     password = serializers.CharField(write_only=True, max_length=120)
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    birthdate = serializers.DateField(default=None)
+    is_employee = serializers.BooleanField(default=False)
+    is_superuser = serializers.BooleanField(default=False, read_only=True)
 
     def validate_username(self, value):
         if User.objects.filter(username=value).exists():
@@ -24,4 +25,6 @@ class UserSerializer(serializers.Serializer):
         return value
 
     def create(self, validated_data: dict) -> User:
+        if validated_data["is_employee"]:
+            return User.objects.create_superuser(**validated_data)  
         return User.objects.create_user(**validated_data)    
